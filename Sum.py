a51389d48a47f664097c0cd487923b727b189b45
farmerlike <73086625+farmerlike@users.noreply.github.com>
2021-07-27 15:52:13 +0800

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -1,7 +1,9 @@
+import os
+
 def work_add(a,b):
     c=int(a)+int(b);
     print(c);
     print(a+"与"+b+"的和为"+str(c));
-first_num= input("Please enter your first number: ");
-second_num = input("Please enter your second number: ");
+first_num= os.getENV("first_num");
+second_num = os.getENV("second_num");
 work_add(first_num,second_num)
