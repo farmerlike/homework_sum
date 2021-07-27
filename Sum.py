d67b9c4a723ace46d1c3ebe80e8cfabefb217e54
farmerlike <73086625+farmerlike@users.noreply.github.com>
2021-07-27 15:54:41 +0800

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -4,6 +4,6 @@ def work_add(a,b):
     c=int(a)+int(b);
     print(c);
     print(a+"与"+b+"的和为"+str(c));
-first_num= os.getENV("first_num");
-second_num = os.getENV("second_num");
+first_num= os.getenv("first_num");
+second_num = os.getenv("second_num");
 work_add(first_num,second_num)
