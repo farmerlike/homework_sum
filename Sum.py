cd154958203744871e80d221fc1fd049f30bfe30
farmerlike <73086625+farmerlike@users.noreply.github.com>
2021-07-27 11:05:15 +0800

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,7 @@
+def work_add(a,b):
+    c=int(a)+int(b);
+    print(c);
+    print(a+"与"+b+"的和为"+str(c));
+first_num= input("Please enter your first number: ");
+second_num = input("Please enter your second number: ");
+work_add(first_num,second_num)
